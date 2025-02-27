fc33cb7c6ad2cbebf91cf018b497185b80dad07a
Julio Cesar <julio.aquino07092002@gmail.com>
2025-02-26 23:17:31 -0300

    comitandoo

--- Scripts/gerarDataCompleta.js
+++ Scripts/gerarDataCompleta.js
@@ -1,7 +1,6 @@
 function gerarDataCompleta(){
   const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"})
   const data = new Date().toLocaleDateString("pt-BR")
-
   const hora = new Date().toLocaleTimeString("pt-BR", { hour: "numeric", minute: "numeric"})
 
   const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
